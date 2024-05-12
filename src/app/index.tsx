b77f18ee0c0392ff7fe82ed9a840250c21fa8af3
Augusto Chissano <shuaiksakur@gmail.com>
2024-05-11 18:00:03 -1200

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+export default function Home() {
+     return (
+          <View className="">
+               <Text>Hey, what's up?</Text>
+               <Text>Hey, what's up?</Text>
+               <Text>Hey, what's up?</Text>    
+          </View>
+     )
+}
---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+     white: "#E1E1E6",
+
+     gray : {
+          10 : "#C4C4CC",
+          200: "#E5E5E5",
+          300: "#8D8D99"
+     }
+}
